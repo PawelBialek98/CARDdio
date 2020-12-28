e619e0882a8556b58d2f9d7c93afcc176d677d42
pawelbialek <pawelbialek98@icloud.com>
2020-12-28 22:17:43 +0100

    added UUIDConverter

---
+++ src/main/java/pl/lodz/p/it/cardio/utils/UUIDConverter.java
@@ -0,0 +1,18 @@
+package pl.lodz.p.it.cardio.utils;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.util.UUID;
+
+@Converter(autoApply = true)
+public class UUIDConverter implements AttributeConverter<UUID, UUID> {
+    @Override
+    public UUID convertToDatabaseColumn(UUID attribute) {
+        return attribute;
+    }
+
+    @Override
+    public UUID convertToEntityAttribute(UUID dbData) {
+        return dbData;
+    }
+}
